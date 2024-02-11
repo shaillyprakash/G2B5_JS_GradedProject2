35ff906a657d8559a75fca50440ebddaafaa71ea
shaillyprakash <130748064+shaillyprakash@users.noreply.github.com>
2024-02-12 01:45:27 +0530

    Add files via upload

---
+++ JavaScript Project/Login.js
@@ -0,0 +1,24 @@
+window.history.forward();
+function noBack() {
+    window.history.forward();
+}
+
+sessionStorage.setItem("credentials", JSON.stringify({name:["shailly","yashi"], passcode: ["123", "456"]}))
+var res = sessionStorage.getItem("credentials")
+console.log(res)
+
+function validate(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    var cred = JSON.parse(res)
+    cred.name.map ( (item, ind)=>{
+        if(username === item && password === cred.passcode[ind]){
+            location.href="Resume.html"
+        }
+        else {
+        document. getElementById ("error").innerHTML = "Invalid username/password"
+        }
+        })
+    }
+    
